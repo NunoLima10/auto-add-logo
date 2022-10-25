4c99f1f6e840ba5570a04397a70aacb535fa4640
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-10-25 17:40:19 -0100

    feat:add  askopenfile and askdirectory

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from src.logo_adder import LogoAdder
 from src.logo_position import LogoPosition
+from tkinter import filedialog
 
 def main() -> None:
-    logo_path = "dot.png"
-    images_folder = "photos"
+    filetypes = (('Images', ('*.png','*.jpg')),)
+    logo_path = filedialog.askopenfile(title='Chose logo',initialdir='/',filetypes=filetypes,).name
+    images_folder = filedialog.askdirectory(title='Chose images',initialdir='/')
 
     logo_adder = LogoAdder(
         logo_path, 
         images_folder, 
-        LogoPosition.top_left, 
+        LogoPosition.bottom_right, 
         logo_size=0.5,
         logo_angle = 0,
-        x_offset = -10,
-        y_offset = -10
+        x_offset = -50,
+        y_offset = -50
     )
     logo_adder.start()
     
